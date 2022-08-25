e40d625e4bfa2e2df8331b199859a02743479a07
Daniel Torres <dtorres@datagrasp.io>
2022-08-24 21:24:34 -0400

    Adding more unit tests

---
+++ TwitterConsole.Test/Services/ConcurrentQueueManagerTest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Twitter.Service.Services;
+
+namespace TwitterConsole.Test.Services
+{
+    [TestFixture()]
+    public class ConcurrentQueueManagerTest
+    {
+        private ILogger<ConcurrentQueueManager> logger;
+        private ConcurrentQueueManager manager;
+
+        [SetUp]
+        public void Setup()
+        {            
+            logger = Mock.Of<ILogger<ConcurrentQueueManager>>();
+            manager = new ConcurrentQueueManager(logger);
+        }
+
+        [Test]
+        public void When_Count_Tweet()
+        {
+            var count = manager.Count();
+            Assert.AreEqual(count, 0);
+        }
+
+        [Test]
+        public void When_Add_Tweet()
+        {
+            manager.AddTweet("first");
+            var count = manager.Count();
+            Assert.AreEqual(count, 1);            
+        }
+
+        [Test]
+        public void When_Get_Tweet()
+        {
+            manager.AddTweet("first");
+            var tweet = manager.GetTweet();
+            Assert.IsNull(tweet);
+        }
+    }
+}
---
+++ TwitterConsole.Test/Services/TwitterRetrieveServiceTest.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using Moq;
+using System.Collections.Generic;
+using Twitter.Client.Interfaces;
+using System.Threading.Tasks;
+using System.Threading;
+using Twitter.Service.Services.Interfaces;
+using Twitter.Service.Services;
+using Microsoft.Extensions.Logging;
+
+namespace TwitterConsole.Test.Services
+{
+    [TestFixture()]
+    public class TwitterRetrieveServiceTest
+    {
+        private ITweetAPI twitterClient;
+        private ITwitterQueueManager queue;
+        private ILogger<TwitterRetrieveService> logger;
+        private ITwitterConsumerService twitterRetrieveService;
+
+        [SetUp]
+        public void Setup()
+        {
+            twitterClient = Mock.Of<ITweetAPI>();
+            queue = Mock.Of <ITwitterQueueManager>();
+            logger = Mock.Of<ILogger<TwitterRetrieveService>>();
+            twitterRetrieveService = new TwitterRetrieveService(logger, twitterClient, queue);
+        }
+
+        [Test]
+        public async Task When_Tweets_Added()
+        {
+            var token = new CancellationToken();
+            var tweets = GetStringsAsync();
+            
+            Mock.Get(queue)
+                .Setup(s => s.AddTweet(It.IsAny<string>()));
+
+            Mock.Get(twitterClient)
+                .Setup(s => s.GetTweetsAsync(It.IsAny<CancellationToken>()))
+                .Returns(tweets);
+
+            await twitterRetrieveService.ConsumeAsync(token);
+
+            Mock.VerifyAll();
+        }
+
+        private static async IAsyncEnumerable<string> GetStringsAsync()
+        {
+            yield return "first";
+            await Task.Delay(1000);
+            yield return "second";
+            await Task.Delay(1000);
+            yield return "third";
+        }
+    }
+}
